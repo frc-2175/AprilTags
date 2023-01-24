fc3556f33f1916f1d16ed10ef6873d13be50e5b9
Nia Ahmad <idrees.s.ahmad@icloud.com>
2023-01-24 15:18:24 -0600

    Fixed capture.py

--- capture.py
+++ capture.py
@@ -1,4 +1,4 @@
-from pupil_apriltags import Detector
+from os.path import exists
 import cv2
   
 imageCount = 0  
@@ -13,13 +13,17 @@ while(True):
 	cv2.imshow('frame', frame)
 	
 	#Define escape key
-	if cv2.waitKey(1) & 0xFF == ord('q'):
+	if cv2.waitKey(81) == ord('q'):
 		break
   
-	if cv2.waitKey(1) & 0xFF == ord('c'):
+	if cv2.waitKey(67) == ord('c'):
 		cv2.imwrite(str(imageCount) + ".jpg", frame)
-
+		print("Capturing...")
+		while not exists(str(imageCount) + ".jpg"):
+			print("Waiting to write file")
+			
 	imageCount += 1
+	
 #Release the video object after loop is complete & destroy the windows
 vid.release()
 cv2.destroyAllWindows()
